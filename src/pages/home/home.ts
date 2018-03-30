9d0216a0a4ee6d48872c2177920f42e12aee3771
dularion <antonia.hulha@systaro.de>
2018-03-31 01:44:47 +0200

    use pages from IFarmProvider instead of duplicate code

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {IFarmProvider} from '../../app/ifarm.app.provider';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  pages:any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private ifarmAppProvider: IFarmProvider) {
+    this.pages = this.ifarmAppProvider.getPages();
+  }
 
+  openPage(page) {
+    this.navCtrl.push(page.component);
   }
 
 }
