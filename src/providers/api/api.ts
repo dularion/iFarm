c544149740240704da7b8a92fc555e63354b3cfa
dularion <antonia.hulha@systaro.de>
2018-04-03 08:08:43 +0200

    WIP: add query method to apiService

--- src/providers/api/api.ts
+++ src/providers/api/api.ts
@@ -1,5 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import * as firebase from "firebase";
+import * as _ from 'lodash';
 
 /**
  * Api is a generic REST Api handler. Set your API url first.
@@ -7,8 +9,10 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class Api {
   url: string = 'https://example.com/api/v1';
+  db;
 
   constructor(public http: HttpClient) {
+    this.db = firebase.firestore();
   }
 
   get(endpoint: string, params?: any, reqOpts?: any) {
@@ -27,6 +31,23 @@ export class Api {
     }
 
     return this.http.get(this.url + '/' + endpoint, reqOpts);
+  }
+
+
+  query(collection: string): Promise<Array<any>>{
+    return new Promise<Array<any>>((resolve, reject) => {
+      this.db.collection(collection).get().then(function(querySnapshot) {
+        console.log(querySnapshot);
+        let result = _.map(querySnapshot, function (doc) {
+          let data = doc.data();
+          data.id = doc.id;
+          return data;
+        });
+
+        resolve(result);
+      }, reject);
+
+    });
   }
 
   post(endpoint: string, body: any, reqOpts?: any) {
