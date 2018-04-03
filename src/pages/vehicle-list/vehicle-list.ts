eb5b5351ff04dfe6b59b2361ca01ae1794aca253
dularion <antonia.hulha@systaro.de>
2018-04-03 08:09:00 +0200

    WIP: use query-method in vehicleListPage

--- src/pages/vehicle-list/vehicle-list.ts
+++ src/pages/vehicle-list/vehicle-list.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {HttpClient} from '@angular/common/http';
 import {VehicleDetailPage} from '../vehicle-detail/vehicle-detail';
+import * as firebase from "firebase";
+import {Api} from '../../providers/api/api';
+
 
 /**
  * Generated class for the VehicleListPage page.
@@ -19,12 +22,28 @@ export class VehicleListPage {
   items: any = [];
   type: string;
 
-  constructor(public navCtrl: NavController, private http: HttpClient) {
+  constructor(public navCtrl: NavController, private http: HttpClient, private api: Api) {
     this.type = '';
   }
 
   ionViewDidLoad(){
     this.http.get('assets/vehicles.json').subscribe(data => this.items = data);
+    this.api.query('vehicles').then((result) => {
+     console.log(result);
+    });
+
+
+    // db.collection("cities").where("capital", "==", true)
+    //   .get()
+    //   .then(function(querySnapshot) {
+    //     querySnapshot.forEach(function(doc) {
+    //       // doc.data() is never undefined for query doc snapshots
+    //       console.log(doc.id, " => ", doc.data());
+    //     });
+    //   })
+    //   .catch(function(error) {
+    //     console.log("Error getting documents: ", error);
+    //   });
   }
 
   itemTapped(event, item) {
