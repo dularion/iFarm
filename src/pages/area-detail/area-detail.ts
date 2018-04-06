bdbc38b0bc090ac72fa16d3a5a530018917ad833
dularion <antonia.hulha@systaro.de>
2018-04-07 00:01:26 +0200

    implement area-detail with FormBuilder and firebase

--- src/pages/area-detail/area-detail.ts
+++ src/pages/area-detail/area-detail.ts
@@ -1,22 +1,41 @@
 import { Component } from '@angular/core';
-import {NavController, NavParams} from 'ionic-angular';
+import {AlertController, NavController, NavParams} from 'ionic-angular';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Api} from '../../providers/api/api';
 
 @Component({
   selector: 'area-detail',
   templateUrl: 'area-detail.html'
 })
 export class AreaDetailPage {
-  selectedItem: any;
+  form: FormGroup;
 
-  constructor(public navParams: NavParams, public navCtrl: NavController) {
-    if(navParams.get('isNew')){
-      this.selectedItem = {}
-    }else{
-      this.selectedItem = navParams.get('item');
-    }
+  constructor(public navParams: NavParams, public navCtrl: NavController, public fb: FormBuilder,
+              public alertCtrl: AlertController, private api: Api) {
+    let existingDoc = navParams.get('item') || {};
+
+    this.form = fb.group({
+      name: [existingDoc.name, Validators.required],
+      nr: [existingDoc.nr, Validators.required],
+      type: [existingDoc.type, Validators.required],
+      size: existingDoc.size,
+      nextCrop: existingDoc.nextCrop
+    });
   }
 
   save(){
-    this.navCtrl.pop();
+    let _this = this;
+    if(_this.form.status == 'INVALID'){
+      let alert = this.alertCtrl.create({
+        title: 'Formular nicht vollständig',
+        subTitle: 'Bitte überprüfen Sie die Pflichtangaben (markiert mit *)',
+        buttons: ['OK']
+      });
+      alert.present();
+      return;
+    }
+    this.api.post('areas', this.form.getRawValue()).then(function(){
+      _this.navCtrl.pop();
+    });
   }
 }
