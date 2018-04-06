6897162727278bd77d6194eff3ab554e074aa22f
dularion <antonia.hulha@systaro.de>
2018-04-07 00:14:12 +0200

    implement firebase & FormBuilder for vehicleDetail

--- src/pages/vehicle-detail/vehicle-detail.ts
+++ src/pages/vehicle-detail/vehicle-detail.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import {AlertController, NavController, NavParams} from 'ionic-angular';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Api} from '../../providers/api/api';
 
 /**
  * Generated class for the VehicleDetailPage page.
@@ -13,18 +15,35 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'vehicle-detail.html',
 })
 export class VehicleDetailPage {
-  selectedItem: any;
+  form: FormGroup;
+  existingDoc: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    if(navParams.get('isNew')){
-      this.selectedItem = {}
-    }else{
-      this.selectedItem = navParams.get('item');
-    }
+  constructor(public navCtrl: NavController, public navParams: NavParams, public fb: FormBuilder,
+              public alertCtrl: AlertController, private api: Api) {
+
+    this.existingDoc = navParams.get('item') || {};
+
+    this.form = fb.group({
+      licencePlate: [this.existingDoc.licencePlate, Validators.required],
+      lastCheckup: this.existingDoc.lastCheckup
+    });
   }
 
   save(){
-    this.navCtrl.pop();
+    let _this = this;
+    if(_this.form.status == 'INVALID'){
+      let alert = this.alertCtrl.create({
+        title: 'Formular nicht vollständig',
+        subTitle: 'Bitte überprüfen Sie die Pflichtangaben (markiert mit *)',
+        buttons: ['OK']
+      });
+      alert.present();
+      return;
+    }
+
+    this.api.post('vehicles', this.form.getRawValue(), this.existingDoc.id).then(function(){
+      _this.navCtrl.pop();
+    });
   }
 
   ionViewDidLoad() {
