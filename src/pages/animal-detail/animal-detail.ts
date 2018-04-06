36e58192e1a154b9084cfc2ab6309328ed2b246d
dularion <antonia.hulha@systaro.de>
2018-04-06 23:41:45 +0200

    rework animalDetail with formBuilder

--- src/pages/animal-detail/animal-detail.ts
+++ src/pages/animal-detail/animal-detail.ts
@@ -1,24 +1,33 @@
-import { Component } from '@angular/core';
-import {ModalController, NavController, NavParams} from 'ionic-angular';
+import {Component} from '@angular/core';
+import {AlertController, ModalController, NavController, NavParams} from 'ionic-angular';
 import {PhotoModalPage} from '../photo-modal/photo-modal';
 import {HttpClient} from '@angular/common/http';
 import {AnimalProvider} from '../../providers/animal/animal';
+import {Api} from '../../providers/api/api';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'animal-detail',
   templateUrl: 'animal-detail.html'
 })
 export class AnimalDetailPage {
-  selectedItem: any;
+  form: FormGroup;
   animals: any;
 
   constructor(public modalCtrl: ModalController, public navParams: NavParams, private http: HttpClient,
-              public navCtrl: NavController, public animalProvider: AnimalProvider) {
-    if(navParams.get('isNew')){
-      this.selectedItem = {}
-    }else{
-      this.selectedItem = navParams.get('item');
-    }
+              public navCtrl: NavController, public animalProvider: AnimalProvider, private api: Api,
+              public fb: FormBuilder, public alertCtrl: AlertController) {
+    let existingDoc = navParams.get('item') || {};
+
+    this.form = fb.group({
+      name: [existingDoc.name, Validators.required],
+      gender: [existingDoc.gender, Validators.required],
+      dateOfBirth: [existingDoc.dateOfBirth, Validators.required],
+      type: existingDoc.type,
+      race: existingDoc.race,
+      mother: existingDoc.mother,
+      childrenCount: existingDoc.childrenCount,
+    });
   }
 
   ionViewDidLoad(){
@@ -33,14 +42,26 @@ export class AnimalDetailPage {
 
 
   save(){
-    this.navCtrl.pop();
+    let _this = this;
+    if(_this.form.status == 'INVALID'){
+      let alert = this.alertCtrl.create({
+        title: 'Formular nicht vollständig',
+        subTitle: 'Bitte überprüfen Sie die Pflichtangaben (markiert mit *)',
+        buttons: ['OK']
+      });
+      alert.present();
+      return;
+    }
+    this.api.post('animals', this.form.getRawValue()).then(function(){
+      _this.navCtrl.pop();
+    });
   }
 
-  getTypeForImage(item) {
-    return this.animalProvider.getTypeForImage(item);
+  getTypeForImage() {
+    return this.animalProvider.getTypeForImage(this.form);
   }
 
   isAdult() {
-    return this.animalProvider.isAdult(this.selectedItem);
+    return this.animalProvider.isAdult(this.form);
   }
 }
