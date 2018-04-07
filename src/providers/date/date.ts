bf309be7a77fa35c1bae818c94eef86c8fcf7a49
dularion <antonia.hulha@systaro.de>
2018-04-07 22:16:51 +0200

    add DateProvider which helps with some common date issues across the app

---
+++ src/providers/date/date.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import moment from 'moment';
+
+/*
+  Generated class for the DateProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class DateProvider {
+
+  constructor(public http: HttpClient) {
+    console.log('Hello DateProvider Provider');
+  }
+
+  public static getIsoStringFromDate(date): string {
+    return date ? date.toISOString() : '';
+  }
+
+  public static isKeyValuePairDate(key, value): boolean {
+    return (key.indexOf('date') > -1 && moment(value).isValid());
+  }
+
+  public static convertDateTimeToDate(value:string): Date {
+    return moment(value).subtract(12, 'h').toDate();
+  }
+
+}
