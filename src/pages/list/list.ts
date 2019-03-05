8b2dcd683e5d924886bd62b68ef0e7ca116d995c
Maxim Bondarenko <maximbndrnk@gmail.com>
2019-03-05 12:07:09 +0200

    add settings to select language

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'page-list',
@@ -9,8 +10,13 @@ export class ListPage {
   selectedItem: any;
   icons: string[];
   items: Array<{title: string, note: string, icon: string}>;
+  showLanguageSettings = false;
+  availableLanguages = ['DE', 'EN'];
+  currentLang = 'DE';
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public translate: TranslateService) {
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedItem = navParams.get('item');
 
@@ -19,13 +25,24 @@ export class ListPage {
     'american-football', 'boat', 'bluetooth', 'build'];
 
     this.items = [];
-    for (let i = 1; i < 11; i++) {
+    for (let i = 1; i < 5; i++) {
       this.items.push({
         title: 'Item ' + i,
         note: 'This is item #' + i,
         icon: this.icons[Math.floor(Math.random() * this.icons.length)]
       });
     }
+    console.log('translate', translate);
+    this.currentLang = translate.currentLang.toLocaleUpperCase();
+  }
+
+  selectLanguage(lang) {
+    console.log('ha',lang);
+    lang = lang.toLocaleLowerCase();
+    this.translate.setDefaultLang(lang);
+    this.translate.use(lang);
+    console.log('translate', this.translate);
+
   }
 
   itemTapped(event, item) {
@@ -33,5 +50,12 @@ export class ListPage {
     this.navCtrl.push(ListPage, {
       item: item
     });
+  }
+
+  toggleSelectLanguage() {
+    this.showLanguageSettings = !this.showLanguageSettings;
+  }
+
+  ionViewDidLoad() {
   }
 }
