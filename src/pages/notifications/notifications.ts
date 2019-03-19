d3124d74480bbdba335a62ea7f45b0a9806aa8e9
Maxim Bondarenko <maximbndrnk@gmail.com>
2019-03-19 16:57:50 +0200

    create notification form, basic markup

--- src/pages/notifications/notifications.ts
+++ src/pages/notifications/notifications.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {IonicPage, NavController, NavParams} from 'ionic-angular';
 import {UsersProvider} from "../../providers/user/users";
+import {FormBuilder, FormControl} from "@angular/forms";
 
 
 @Component({
@@ -13,17 +14,39 @@ export class NotificationsPage {
   teamUsers;
   currentUser;
   isLoading = true;
+  form;
+  errorMessage = '';
 
   constructor(public navCtrl: NavController,
               private usersProvider: UsersProvider,
+              private fb:FormBuilder,
               public navParams: NavParams) {
     this.currentUser = usersProvider.getCurrentUserFromUsers();
     this.teamUsers = usersProvider.getMyFamilyUsers();
+    this.isLoading = false;
     console.log('in notificPAGE', this.currentUser,this.teamUsers);
+    this.form = this.createForm();
   }
 
   ionViewDidLoad() {
     console.log('NotificationsPage', this.navParams.data);
   }
 
+
+  createForm(){
+    return this.fb.group({
+      title: new FormControl('',[]),
+      description: new FormControl('',[]),
+      date: new FormControl(new Date(),[]),
+      usersToNotify: new FormControl(),
+    });
+  }
+
+  isInvalidFormField(field) {
+    return this.form.get(field).invalid && this.form.get(field).touched;
+  }
+
+  createNotification(){
+    console.log('createNotification', this.form.value, this.teamUsers);
+  }
 }
